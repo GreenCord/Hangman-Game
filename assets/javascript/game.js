497b549bb81103a34c5bc6bfad1d69ce66a6f7fa
GreenCord <steve@nevizen.com>
2017-10-29 11:53:53 -0400

    Updating html and some javascript hooks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,64 @@
 console.log('Loaded js.');
-alert('sdfkjashf');
+
 // Initialize everything used for the game
+
+
+// functions
+function toggleThis(objID,state) {
+	document.getElementById(objID).style.display = state;
+};
+
+// Word/Phrase list
+var words = [
+	"",
+	"",
+];
+
+// Game object
+var game = {
+	word: "",
+	wins: 0,
+	losses: 0,
+	phase: 'instructions'
+};
+
+var instructions = document.getElementById('instructions');
+
+
+// debug
+console.log(game.word);
+console.log(game.wins);
+console.log(game.losses);
+
+// Start Game
+document.onkeyup = function(event) {
+	var userPressed = event.key
+	if (game.phase === "instructions") {
+		// Instructions are displaying, game has ended or not yet begun
+		switch (userPressed.toLowerCase()) {
+			case '1':
+				console.log('User pressed 1');
+				toggleThis('instructions','none');
+				toggleThis('onTrail','block');
+				game.phase = 'playing';
+				break;
+			case '2':
+				console.log('User pressed 2');
+				break;
+			case '3':
+				console.log('User pressed 3');
+				break;
+			default:
+				console.log('User pressed something else');
+		};
+	} else if (game.phase === "playing") {
+		console.log(userPressed.toLowerCase());
+		// Game is underway
+	} else {
+		alert('Something is wrong.')
+	}
+} 
+
 
 // Select word/phrase to use for game
 
