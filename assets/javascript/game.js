bf4dc3786011f95bbe61b97448254ab869f7b4c2
GreenCord <steve@nevizen.com>
2017-10-29 10:07:03 -0400

    Linking assets, added basic pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+console.log('Loaded js.');
+alert('sdfkjashf');
+// Initialize everything used for the game
+
+// Select word/phrase to use for game
+
+// Display blanks for word
+
+// Collect user input
+
+// Check if user input is in word/phrase
+
+// If it is, display
+
+// If not, draw hangman part
+
+// If word/phrase fully guessed, record win
+
+// If hangman fully drawn, record loss
