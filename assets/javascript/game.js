d9f1784a69486d23a9090401b5665d1bcddfdf0c
GreenCord <steve@nevizen.com>
2017-10-30 23:28:05 -0400

    Refactored, cleaned up functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 // Initialize everything used for the game
 
-var wordArr = [];
-
 var game = {
 	word: "",
+	wordArr: [],
+	lettersGuessedCorrectly: 0,
+	guessedArr: [],
 	wins: 0,
 	losses: 0,
 	phase: 'instructions',
@@ -13,6 +14,7 @@ var game = {
 	child2: { name : "Eunice", gender : "female", alive: true },
 	child3: { name : "Enos", gender : "male", alive: true },
 	child4: { name : "Winifred", gender : "female", alive: true },
+	lives: 5,
 	letterding: new Audio('./assets/sounds/ding.wav'),
 	familydeath: new Audio('./assets/sounds/dead.wav'),
 	loss: new Audio('./assets/sounds/loss.wav'),
@@ -24,22 +26,27 @@ var game = {
 	updateUserInput: function(objID,key) {
 		document.getElementById(objID).innerText = key;
 		},
-	toggle: function(hideID,showID,gamePhase,gameStatus) {
-		document.getElementById(hideID).style.display = 'none';
-		document.getElementById(showID).style.display = 'block';
-		this.phase = gamePhase;
-		if (gameStatus != undefined) {
-			game.updateUserInput('gamestatus',gameStatus);
+	toggle: function(hideID,showID,gamePhase,gameStatus) 
+		{
+			document.getElementById(hideID).style.display = 'none';
+			document.getElementById(showID).style.display = 'block';
+			this.phase = gamePhase;
+			if (gameStatus != undefined) {
+				game.updateUserInput('gamestatus',gameStatus);
+			}
+		},
+	killFamily: function(index)
+		{
+			var ul = document.getElementById('familyList');
+			var items = ul.getElementsByTagName('li');
+			items[index].classList.remove("alive");
+			items[index].classList.add("dead");
 		}
-	}
 };
+
+// initializing family
 
 var arrFamily = [game.player, game.spouse, game.child1, game.child2, game.child3, game.child4];
-
-// functions
-function toggleThis(objID,state) {
-	document.getElementById(objID).style.display = state;
-};
 
 function initializeFamily(listId) {
 	var ul = document.getElementById(listId);
@@ -53,78 +60,61 @@ function initializeFamily(listId) {
 	}
 };
 
-function killFamily(index) {
-	var ul = document.getElementById('familyList');
-	var items = ul.getElementsByTagName('li');
-	items[index].classList.remove("alive");
-	items[index].classList.add("dead");
-};
-
-// function updateUserInput(objID,key) {
-// 	document.getElementById(objID).innerText = key;
-// }
-
-// Word/Phrase list
-
-
-
 // Start Game
 initializeFamily('setupFamily');
-var lives = 5;
-var lettersGuessedCount = 0;
-var guessedArr = [];
 
-document.onkeyup = function(event) {																// Detect user input based on game phase
+
+document.onkeyup = function(event) {		// Detect user input based on game phase
 	var userPressed = event.key
 
-	if (game.phase === "instructions") {															// Instructions are displaying
+	if (game.phase === "instructions") {	// Instructions are displaying
 
 		switch (userPressed.toLowerCase()) {														// user chooses to play game
 			case '1':
 				
 				// initialize and begin play
 				initializeFamily('familyList');
-				lives = 5;
-				lettersGuessedCount = 0;
-				guessedArr = [];
-				document.getElementById('userInput').innerText = "Letters guessed: ";
+				game.lives = 5;
+				game.lettersGuessedCorrectly = 0;
+				game.guessedArr = [];
+				game.updateUserInput('userInput','Letters guessed: ');
+
 				// Select word/phrase to use for game
 				var gameWord = game.words[Math.floor(Math.random()*game.words.length)];
 				
 				// Display blanks for word
-				// put each letter into an array
-				wordArr = gameWord.split('');
-			
-				
-				// create <li>s in ul#wordDisplay
-				// update <li>s widths
-				liWidth = 100 / wordArr.length;
+				game.wordArr = gameWord.split(''); // put each letter into an array
+				liWidth = 100 / game.wordArr.length; // create <li>s in ul#wordDisplay, update <li>s widths
 				var ul = document.getElementById('wordDisplay');
 				var items = [];
-				for (var i = 0; i < wordArr.length; i++) {
+				for (var i = 0; i < game.wordArr.length; i++) {
 					items += '<li style="width: ' + liWidth + ';">&mdash;</li>'
 				}
 				ul.innerHTML = items;
-				game.toggle('instructions','onTrail','playing');
+				game.toggle('instructions','onTrail','playing'); // change to onTrail panel to begin play
 				break;
 			
-																																		// user choose to setup family
-			case '2':
-				game.toggle('instructions','setup','setup');
+																																		
+			case '2': 																										// user chooses to setup family
+				game.toggle('instructions','setup','setup'); // change to setup panel
 				break;
 			
 																																		// user chooses to view stats
 			case '3':
-				if ((game.wins > 1) || (game.wins === 0)) {
-					document.getElementById('winCount').innerText = game.wins + " times";
+				var _winCount = document.getElementById('winCount');
+				var _lossCount = document.getElementById('lossCount');
+
+				if (game.wins === 1) {   // updates stats panel with win/loss counters
+					_winCount.innerText = game.wins + " time";
 				} else {
-					document.getElementById('winCount').innerText = game.wins + " time";
+					_winCount.innerText = game.wins + " times";
 				}
-				if ((game.losses > 1) || (game.losses === 0)) {
-					document.getElementById('lossCount').innerText = game.losses + " times";
+				if (game.losses === 1) {
+					_lossCount.innerText = game.losses + " time";
 				} else {
-					document.getElementById('lossCount').innerText = game.losses + " time";
+					_lossCount.innerText = game.losses + " times";
 				}
+
 				game.toggle('instructions','stats','stats');
 				break;
 			
@@ -153,8 +143,8 @@ document.onkeyup = function(event) {																// Detect user input based o
 
 		// loop through wordArr and count/display matches, record guessed letters
 		var matchCount = 0;
-		var a = guessedArr.indexOf(currentLetter);
-		wordArr.forEach(function(letter){ // for each item in the array, check if it matches guessed letter
+		var a = game.guessedArr.indexOf(currentLetter);
+		game.wordArr.forEach(function(letter){ // for each item in the array, check if it matches guessed letter
 			if (currentLetter === letter) { // if it matches
 				matchCount++;									// add 1 to match count
 			}
@@ -165,20 +155,20 @@ document.onkeyup = function(event) {																// Detect user input based o
 			var ul = document.getElementById('wordDisplay');
 			var items = ul.getElementsByTagName('li');
 			if (a < 0) {
-				for (var i = 0; i < wordArr.length; i++) {
-					if (wordArr[i] === currentLetter) {
+				for (var i = 0; i < game.wordArr.length; i++) {
+					if (game.wordArr[i] === currentLetter) {
 						items[i].innerText = currentLetter.toUpperCase();
-						lettersGuessedCount++;
+						game.lettersGuessedCorrectly++;
 						if (a < 0) {	// if guessed letter is not in the array
-							guessedArr.push(currentLetter); // add the letter to the array
-							a = guessedArr.indexOf(currentLetter);
+							game.guessedArr.push(currentLetter); // add the letter to the array
+							a = game.guessedArr.indexOf(currentLetter);
 							document.getElementById('userInput').innerText += currentLetter.toUpperCase() + ' '; //add to visible letters guessed
 							game.letterding.play(); //play letter ding sound
 						}
 					}
 				}
 			}
-			if (wordArr.length === lettersGuessedCount) { // Detect win condition - when letters guessed equals the length of the word
+			if (game.wordArr.length === game.lettersGuessedCorrectly) { // Detect win condition - when letters guessed equals the length of the word
 				game.win.play();
 				game.wins++;
 				game.toggle('onTrail','instructions','instructions','You made it to Oregon! Hooray.');
@@ -186,16 +176,16 @@ document.onkeyup = function(event) {																// Detect user input based o
 
 		} else {
 			if (a < 0) {
-				guessedArr.push(currentLetter);
+				game.guessedArr.push(currentLetter);
 				document.getElementById('userInput').innerText += currentLetter.toUpperCase() + ' ';
 				// kill family member
-				killFamily(lives);
-				lives--;
-				if (lives < 0) { // No more family members, user loses
+				game.killFamily(game.lives);
+				game.lives--;
+				if (game.lives < 0) { // No more family members, user loses
 					game.loss.play();
 					game.losses++;
 					game.toggle('onTrail','instructions','instructions','You didn\'t make it to Oregon.');
-				} else if (lives >= 0) { // Still family members left
+				} else if (game.lives >= 0) { // Still family members left
 					game.familydeath.play();
 				}
 			}
@@ -203,21 +193,15 @@ document.onkeyup = function(event) {																// Detect user input based o
 
 		// end user detection for game mode
 
-	} else if (game.phase === "stats") {
+	} else if (game.phase === "stats") {															// user is in stats mode
 		// user is in stats mode
 		
 		// Collect user input
 		switch (userPressed.toLowerCase()) {
 			default:
-				//updateUserInput('userInput',userPressed);
-				// toggleThis('stats','none');
-				// toggleThis('instructions', 'block');
-				// game.phase = 'instructions';
 				game.toggle('stats','instructions','instructions');
 				break;
 		}
 	} 
 
 } // end detects user keypress
-
-
